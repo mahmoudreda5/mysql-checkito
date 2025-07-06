a763c67a6024cda64093be080f4405ceb3d4d3ce
mahmoud reda <r.engmahmoud7@gmail.com>
2025-07-06 11:28:14 +0200

    data distribution demo

--- demos/distro.sql
+++ demos/distro.sql
@@ -1,9 +1,38 @@
 USE checkito;
 
+-- show table size and data
+SELECT COUNT(*) FROM distro;
+SELECT * FROM distro LIMIT 20;
+
+
+-- show table indexes
 SHOW INDEXES FROM distro;
 
--- fool optimizer?, not really...
-EXPLAIN SELECT * FROM distro WHERE a > 0;
+-- 'a' column selectivity
+SELECT COUNT(DISTINCT a) / COUNT(*) FROM distro;
 
--- selectivity is 1 / 10000, bad index
+
+-- choose a threshold for the 'a' column
+SET @threshold = (SELECT MIN(a) FROM (SELECT a FROM distro ORDER BY a DESC LIMIT 20000) AS t);
+SELECT @threshold;
+
+
+-- fool optimizer?, not really...
+EXPLAIN SELECT * FROM distro WHERE a > @threshold;
+EXPLAIN ANALYZE SELECT * FROM distro WHERE a > @threshold;
+EXPLAIN ANALYZE SELECT * FROM distro force index (idx_a) WHERE a > @threshold;
+
+
+-- 'b' column selectivity
+SELECT COUNT(DISTINCT b) / COUNT(*) FROM distro;
+
+-- wrong optimizer decision, use terminal to see the difference
 EXPLAIN SELECT * FROM distro WHERE b = 1;
+EXPLAIN ANALYZE SELECT * FROM distro WHERE b = 1;
+
+EXPLAIN SELECT * FROM distro ignore index (idx_b) WHERE b = 1;
+EXPLAIN ANALYZE SELECT * FROM distro ignore index (idx_b) WHERE b = 1;
+
+EXPLAIN SELECT * FROM distro WHERE b = 0;
+EXPLAIN ANALYZE SELECT * FROM distro WHERE b = 0;
+SELECT * FROM distro WHERE b = 0;
