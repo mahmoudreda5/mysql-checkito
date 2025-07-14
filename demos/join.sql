d4ca1920c1fa0075c3d0579f126166a2b9610d4e
mahmoud reda <r.engmahmoud7@gmail.com>
2025-07-14 14:42:12 +0200

    join demos, join ordering

--- demos/join.sql
+++ demos/join.sql
@@ -17,7 +17,7 @@ SHOW INDEXES FROM users;
 SHOW INDEXES FROM accounts;
 
 
--- join wth no indexes, not really there is still an index (pk)
+-- join with no indexes, not really there is still an index (pk)
 SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id;
 EXPLAIN SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id;
 EXPLAIN FORMAT=JSON SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id;
@@ -61,3 +61,45 @@ EXPLAIN FORMAT=TREE SELECT * FROM users INNER JOIN employees ON users.age = empl
 EXPLAIN ANALYZE SELECT * FROM users INNER JOIN employees ON users.age = employees.age;
 ALTER TABLE users ALTER INDEX idx_age INVISIBLE;
 ALTER TABLE employees ALTER INDEX idx_age INVISIBLE;
+
+
+-- Hash join
+EXPLAIN FORMAT=TREE SELECT /*+ HASH_JOIN(users employees) */ * FROM users 
+INNER JOIN employees ON users.age = employees.age where users.name LIKE 'mah%' AND employees.age < 100000;
+
+
+-- join order
+
+ALTER TABLE users ALTER INDEX idx_age VISIBLE;
+ALTER TABLE employees ALTER INDEX idx_age VISIBLE;
+
+-- normally smaller table is the outer table
+EXPLAIN format=tree SELECT * FROM users INNER JOIN employees ON users.age = employees.age WHERE users.age < 30;
+EXPLAIN format=tree SELECT * FROM users INNER JOIN employees ON users.age = employees.age WHERE employees.age < 30;
+
+-- join order changes based on filtering
+ALTER TABLE accounts ALTER INDEX idx_user_id VISIBLE;
+ALTER TABLE accounts ALTER INDEX idx_balance VISIBLE;
+EXPLAIN SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id WHERE accounts.balance < 1000;
+EXPLAIN SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id WHERE users.age < 20;
+EXPLAIN SELECT * FROM users INNER JOIN accounts ON users.id = accounts.user_id WHERE users.age < 20 AND accounts.balance < 1000;
+
+
+-- join order will change based on order by
+EXPLAIN SELECT * FROM users
+INNER JOIN accounts ON users.id = accounts.user_id
+WHERE users.age < 20 AND accounts.balance < 10000 ORDER BY users.id;
+
+EXPLAIN ANALYZE SELECT * FROM users
+INNER JOIN accounts ON users.id = accounts.user_id
+WHERE users.age < 20 AND accounts.balance < 10000 ORDER BY users.id;
+
+-- again optimizer is not choosing the most optimal plan!
+EXPLAIN ANALYZE SELECT * FROM users
+STRAIGHT_JOIN accounts ON users.id = accounts.user_id
+WHERE users.age < 20 AND accounts.balance < 10000 ORDER BY users.id;
+
+-- pipelined order by
+EXPLAIN SELECT * FROM users
+INNER JOIN accounts ON users.id = accounts.user_id
+WHERE users.age < 20 AND accounts.balance < 10000 ORDER BY users.id limit 20;
